bc7f198132f68267079c1ed58eb6ca469a3964d3
Fernando Munoz <nando.munalv@gmail.com>
2022-10-07 13:48:22 -0500

    first commit

---
+++ src/main/java/com/example/simplespringsecurityjwtproject/app/SimpleSpringsecurityJwtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplespringsecurityjwtproject.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringsecurityJwtProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringsecurityJwtProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplespringsecurityjwtproject/app/SimpleSpringsecurityJwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplespringsecurityjwtproject.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringsecurityJwtProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
